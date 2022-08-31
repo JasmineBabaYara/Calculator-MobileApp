30d60384d07a6aa6ba8b3d70bb7a2698edf57a05
JasmineBabaYara <jassyprincess1@icloud.com>
2022-08-30 18:18:28 -0700

    no styling included

--- components/Sum.js
+++ components/Sum.js
@@ -3,6 +3,25 @@ import { View, TextInput, TouchableOpacity, Text } from "react-native";
 import styles from "./style";
 
 export default function Sum() {
-  return <View></View>
-}
+  const [input, setInput] = useState("");
+  const [inputarr, setInputArr] = useState([]);
 
+  const displayvalue = () => {
+    // console.log(input)
+    setInputArr(input);
+  };
+
+  return (
+    <View>
+      <TextInput
+        onChangeText={(value) => setInput(value)}
+        placeholder="type here"
+      />
+
+      <TouchableOpacity onPress={displayvalue}>
+        <Text>calculate</Text>
+      </TouchableOpacity>
+      <Text>{inputarr}</Text>
+    </View>
+  );
+}
