b669fc2f0a394909db45449281ba8abbba71cf84
JasmineBabaYara <jassyprincess1@icloud.com>
2022-09-11 22:23:01 -0700

    finalized

--- components/Calculate.js
+++ components/Calculate.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { View, TextInput, TouchableOpacity, Text } from "react-native";
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  SafeAreaView,
+} from "react-native";
 import styles from "./style";
 
 export default function Calculate() {
@@ -21,95 +27,153 @@ export default function Calculate() {
   };
 
   return (
-    <View style={{ margin: 100 }}>
-
+    <SafeAreaView style={{ height: "100%", width: "100%" }}>
       {/* display container */}
-      <View>
-      <Text>{inputarr}</Text>
-      <Text>{result}</Text>
+      <View style={styles.displaycontainer}>
+        <Text style={styles.input}>{inputarr}</Text>
+        <Text style={styles.result}>{result}</Text>
       </View>
 
-      {/* first row */}
-      <View style={styles.row}>
-        <TouchableOpacity onPress={clear}>
-          <Text>AC</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("+/-")}>
-          <Text>+/-</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("％")}>
-          <Text>％</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("/")}>
-          <Text>÷</Text>
-        </TouchableOpacity>
-      </View>
+      <View style={styles.keyboard}>
+        <View style={styles.sections}>
+          {/* first row */}
+          <View style={styles.row}>
+            <TouchableOpacity onPress={clear} style={styles.graybtn}>
+              <Text style={styles.text}>AC</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("+/-")}
+              style={styles.graybtn}
+            >
+              <Text style={styles.text}>+/-</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("％")}
+              style={styles.graybtn}
+            >
+              <Text style={styles.text}>％</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("/")}
+              style={styles.violetbtn}
+            >
+              <Text style={styles.text}>÷</Text>
+            </TouchableOpacity>
+          </View>
 
-      {/* second row */}
-      <View style={styles.row}>
-        <TouchableOpacity onPress={() => handleCharacter("7")}>
-          <Text>7</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("8")}>
-          <Text>8</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("9")}>
-          <Text>9</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("*")}>
-          <Text>×</Text>
-        </TouchableOpacity>
-      </View>
+          {/* second row */}
+          <View style={styles.row}>
+            <TouchableOpacity
+              onPress={() => handleCharacter("7")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("8")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>8</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("9")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>9</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("*")}
+              style={styles.violetbtn}
+            >
+              <Text style={styles.text}>×</Text>
+            </TouchableOpacity>
+          </View>
 
-      {/* third row */}
-      <View style={styles.row}>
-        <TouchableOpacity onPress={() => handleCharacter("4")}>
-          <Text>4</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("5")}>
-          <Text>5</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("6")}>
-          <Text>6</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("-")}>
-          <Text>-</Text>
-        </TouchableOpacity>
-      </View>
+          {/* third row */}
+          <View style={styles.row}>
+            <TouchableOpacity
+              onPress={() => handleCharacter("4")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("5")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("6")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("-")}
+              style={styles.violetbtn}
+            >
+              <Text style={styles.text}>-</Text>
+            </TouchableOpacity>
+          </View>
 
-      {/* fourth row */}
-      <View style={styles.row}>
-        <TouchableOpacity onPress={() => handleCharacter("1")}>
-          <Text>1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("2")}>
-          <Text>2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("3")}>
-          <Text>3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("+")}>
-          <Text>+</Text>
-        </TouchableOpacity>
-      </View>
+          {/* fourth row */}
+          <View style={styles.row}>
+            <TouchableOpacity
+              onPress={() => handleCharacter("1")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("2")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("3")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("+")}
+              style={styles.violetbtn}
+            >
+              <Text style={styles.text}>+</Text>
+            </TouchableOpacity>
+          </View>
 
-      {/* fifth row */}
-      <View style={styles.row}>
-        <TouchableOpacity onPress={() => setInputArr(inputarr.slice(0, -1))}>
-          <Text>⌫</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter("0")}>
-          <Text>0</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => handleCharacter(".")}>
-          <Text>.</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => getResult()}>
-          <Text>=</Text>
-        </TouchableOpacity>
+          {/* fifth row */}
+          <View style={styles.row}>
+            <TouchableOpacity
+              onPress={() => setInputArr(inputarr.slice(0, -1))}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>⌫</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter("0")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>0</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCharacter(".")}
+              style={styles.whitebtn}
+            >
+              <Text style={styles.text2}>.</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => getResult()}
+              style={styles.violetbtn}
+            >
+              <Text style={styles.text}>=</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
-
-      
-    </View>
+    </SafeAreaView>
   );
 }
--- components/style.js
+++ components/style.js
@@ -1,25 +1,90 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    row: {
-        maxWidth: '100%',
-        flexDirection: "row",
-        justifyContent:'space-between'
-    },
+  displaycontainer: {
+    margin: 30,
+    height: "30%",
+  },
 
-    screenSecondNumber: {
-        fontSize: 40,
-        color: 'gray',
-        fontWeight: '200',
-        alignSelf: "flex-end",
-    },
+  input: {
+    fontSize: 20,
+    textAlign: "right",
+    marginTop: 120,
+    letterSpacing: 1.5,
+  },
 
-    screenFirstNumber: {
-        fontSize: 96,
-        color: 'gray',
-        fontWeight: '200',
-        alignSelf: "flex-end",
+  result: {
+    fontSize: 50,
+    textAlign: "right",
+    fontWeight: "bold",
+    marginTop: 20,
+  },
+
+  keyboard: {
+    position: "absolute",
+    top: "38%",
+    width: "100%",
+    height: "100%",
+    borderWidth: 1,
+    borderColor: "#fff",
+    borderRadius: 30,
+    shadowColor: "#000",
+    shadowOpacity: 0.6,
+    shadowRadius: 3,
+    shadowOffset: {
+      height: 0.5,
+      width: 0,
     },
-})
+    elevation: 1,
+    backgroundColor: "#f5f5f5",
+  },
+
+  sections: {
+    margin: 30,
+  },
+
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+
+  graybtn: {
+    backgroundColor: "#4E505F",
+    width: 82,
+    height: 82,
+    borderRadius: 28,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  violetbtn: {
+    backgroundColor: "#8a2be2",
+    width: 82,
+    height: 82,
+    borderRadius: 28,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  whitebtn: {
+    backgroundColor: "#fff",
+    width: 82,
+    height: 82,
+    borderRadius: 28,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  text: {
+    fontSize: 30,
+    color: "#fff",
+  },
+
+  text2: {
+    fontSize: 30,
+    color: "#000",
+  },
+});
 
 export default styles;
